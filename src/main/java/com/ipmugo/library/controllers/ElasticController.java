514ccc58df42186acf8ed8c1341608e0e7734418
DDV2412 <dhyanputra24@gmail.com>
2022-12-30 08:32:53 +0700

    Fix Method Request

--- src/main/java/com/ipmugo/library/controllers/ElasticController.java
+++ src/main/java/com/ipmugo/library/controllers/ElasticController.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +30,7 @@ public class ElasticController {
         @Autowired
         private ElasticSearchService elasticSearchService;
 
-        @GetMapping
+        @PostMapping
         public ResponseEntity<ResponseElastic<List<ArticleElastic>>> search(
                         @RequestBody String query) {
                 ResponseElastic<List<ArticleElastic>> responseData = new ResponseElastic<>();
